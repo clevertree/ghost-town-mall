947d7f09172f47c941b0813dabc9f338f9a0ef90
Ari Asulin <ari.asulin@gmail.com>
2016-11-06 04:10:23 +0000

    webgl basics

---
+++ game/engine.js
@@ -0,0 +1,82 @@
+window.engine = (function(){
+	var elmCanvas = [];
+	var glContext;
+	var fragmentShader;
+	var vertexShader;
+	var shaderProgram;
+
+	function engine() {
+
+	}
+
+	engine.prototype.addCanvas = function(canvas) {
+		elmCanvas.push(canvas);
+	};
+
+	engine.prototype.getGLContext = function() {
+		if(glContext)
+			return glContext;
+
+		for(var i=0; i<elmCanvas.length; i++) {
+			var canvas = elmCanvas[i];				
+	            	glContext = canvas.getContext("experimental-webgl");
+        	    // viewportWidth = canvas.width;
+	            // viewportHeight = canvas.height;
+			return glContext;
+		}
+
+		throw new Error("Could not initialise WebGL, sorry :-(");		
+	}
+
+	engine.prototype.addShaderScript = function(scriptElm) {
+		var gl = this.getGLContext();
+
+	        var str = "";
+        	var k = scriptElm.firstChild;
+	        while (k) {
+            		if (k.nodeType == 3) {
+                		str += k.textContent;
+	            	}
+        		k = k.nextSibling;
+	        }
+
+	        var shader;
+        	if (scriptElm.type == "x-shader/x-fragment") {
+            		fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+	        } else if (scriptElm.type == "x-shader/x-vertex") {
+            		vertexShader = gl.createShader(gl.VERTEX_SHADER);
+        	} else {
+            		throw new Error("Invalid Shader Script Type: " + scriptElm.type);
+ 		}
+
+		console.info("Loading and compiling Shader", scriptElm.type, shader);
+	        gl.shaderSource(shader, str);
+        	gl.compileShader(shader);
+
+	        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) 
+            		throw new Error(gl.getShaderInfoLog(shader));
+
+		if(fragmentShader && vertexShader) {
+			console.info("Initializing Shaders");
+
+        		shaderProgram = glContext.createProgram();
+	        	gl.attachShader(shaderProgram, vertexShader);
+        		gl.attachShader(shaderProgram, fragmentShader);
+        		gl.linkProgram(shaderProgram);
+
+		        if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) 
+			        throw new Error("Could not initialise shaders");
+        
+		        gl.useProgram(shaderProgram);
+
+		        shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+		        gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+		        shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+		        shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+		}
+	}
+
+	return new engine();
+})();
+
